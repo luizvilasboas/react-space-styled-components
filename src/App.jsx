015f43670625f8243b0de2692bf43498967b70e8
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-01-27 11:59:16 -0300

    Add gradient background

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { styled } from "styled-components"
+import GlobalStyles from "./components/styles/GlobalStyles"
+
+const GradientBackground = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100vw;
+  height: 100vh;
+`
+
 function App() {
   return (
-    <>
-
-    </>
+    <GradientBackground>
+      <GlobalStyles />
+    </GradientBackground>
   )
 }
 
